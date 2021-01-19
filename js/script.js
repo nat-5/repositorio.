f763d02f071598054a269a474b2ffb875730dbe9
nat-5 <71806085+nat-5@users.noreply.github.com>
2021-01-18 19:44:20 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// VARIABLES Y TIPOS DE DATOS EN JAVASCRIPT
+var   saludo   =   "Hola JavaScript"
+var   edad   =   26
+var   unPoco   =   1.5
+var   siNo   =   true
+var   seccion   =   'a'
+
+// MOSTRAR UN DATO EN CONSOLO
+consola.log(saludo)
+
+
+var datoApellido= document.getElementById('favorito')
